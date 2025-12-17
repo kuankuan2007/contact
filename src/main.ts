b0ee278a4af26ca6d85de69ac1510f0c792d6c59
kuankuan <2163826131@qq.com>
2025-12-18 06:51:05 +0800

    支持/furry路径重定向到#furry

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,22 @@ import { i18n } from './i18n';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { emitEvent } from './scripts/event';
-import { theme, themeValue } from '@/scripts/theme.ts';
 
 window.addEventListener('error', (e) => {
   emitEvent(e.toString(), 'warn');
 });
+
+(() => {
+  try {
+    const path = window.location.pathname;
+    if (path !== '/') {
+      const hash = path.replace(/^\//, '#');
+      const search = window.location.search;
+      const newUrl = `/${search}${hash}`;
+      history.replaceState(null, '', newUrl);
+    }
+  } catch {}
+})();
 
 const app = createApp(App);
 app.use(i18n);
